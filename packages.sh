37b66ed72dab98b20fa64f26aee4836633eeee4b
tacsi566 <45876961+tacsi566@users.noreply.github.com>
2023-03-25 14:28:27 +0100

    Add files via upload

---
+++ packages.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sudo apt-get update
+sudo apt-get install openocd
+sudo apt-get install gcc-multilib
+sudo apt-get install build-essential
+sudo apt-get install python-serial
+sudo apt-get install libudev-dev
+sudo apt-get install moserial
